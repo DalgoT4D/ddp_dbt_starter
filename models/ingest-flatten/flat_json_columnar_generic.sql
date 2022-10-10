924736e943c2e6a3e0450290e641bdeeee857d49
vinod-rajasekaran <airvinod@gmail.com>
2022-10-10 10:09:37 +0530

    column flattening view creation is now supported
    still a WIP as reference into view creation is not working

--- models/ingest-flatten/flat_json_columnar_generic.sql
+++ models/ingest-flatten/flat_json_columnar_generic.sql
@@ -1,11 +1,13 @@
 {{ config(
-    materialized='table',
+    materialized='view',
     indexes=[
       {'columns': ['data_id'], 'type': 'hash'}
     ]
     ) }}
 
---WIP - direct SQL in TablePlus works but not through dbt 
+--WIP -  dbt - issue with using reference 
 --depends_on: {{ ref('unnest_json') }}
+
+--select create_jsonb_flat_view({{ref('unnest_json')}}, 'data_id', 'data')
 
 select create_jsonb_flat_view('"dbt_test".unnest_json', 'data_id', 'data')
