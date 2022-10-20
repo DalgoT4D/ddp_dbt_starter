3ed059b8a4d211f37eccf2441ab95cc78782034d
siddhant <ssiddhant3030@gmail.com>
2022-10-20 19:22:29 +0530

    added the macro

---
+++ macros/flattern_json.sql
@@ -0,0 +1,25 @@
+{% macro flatten_json(model_name, json_column) %}
+  
+{% set survey_methods_query %}
+
+SELECT DISTINCT(jsonb_object_keys({{json_column}})) as column_name 
+from {{model_name}}
+{% endset %}
+
+{% set results = run_query(survey_methods_query) %}
+
+{% if execute %}
+{# Return the first column #}
+{% set results_list = results.columns[0].values() %}
+{% else %}
+{% set results_list = [] %}
+{% endif %}
+
+select
+_airbyte_ab_id,
+_airbyte_data,
+{% for column_name in results_list %}
+_airbyte_data->>'{{ column_name }}' as {{ column_name }}{% if not loop.last %},{% endif %}
+{% endfor %}
+from {{model_name}}
+{% endmacro %}
---
+++ models/ingest-flatten/flatten_survey_data.sql
@@ -0,0 +1,13 @@
+{{ config(
+  materialized='table',
+   indexes=[
+      {'columns': ['_airbyte_ab_id'], 'type': 'hash'}
+    ]
+
+) }}
+{{
+    flatten_json(
+        model_name = source('survey-cto', 'raw_surveycto'),
+        json_column = '_airbyte_data'
+    )
+}}
