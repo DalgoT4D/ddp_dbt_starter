952fe6a5ca5d4aa30db6b0771c299859cc8f3241
siddhant <ssiddhant3030@gmail.com>
2022-10-21 23:08:06 +0530

    flatten the data

--- models/ingest-flatten/flatten_survey_data.sql
+++ models/ingest-flatten/flatten_survey_data.sql
@@ -10,6 +10,7 @@
 {{
     flatten_json(
         model_name = source('survey-cto', 'raw_surveycto'),
-        json_column = '_airbyte_data'
+        json_column = '_airbyte_data',
+        form = 'baseline_ig'
     )
 }}
